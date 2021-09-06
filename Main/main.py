87a056545b5dc37cbd16a31f3250c9145c29d2e3
David <lisaloerinci@gmail.com>
2021-09-06 20:25:38 +0200

    Source von Main getrennt

--- Main/main.py
+++ Main/main.py
@@ -1,16 +1,11 @@
-"""
-(1) Umrechnung von Celsius nach Fahrenheit
-(2) Umrechnung von Celsius nach Kelvin
-(3) Umrechnung von Kelvin nach Celsius
-(4) Umrechnung von Kelvin nach Fahrenheit
-(5) Umrechnung von Fahrenheit nach Celsius
-(6) Umrechnung von Fahrenheit nach Kelvin """
-
 import tkinter
-
-from oberflaeche import Oberflaeche
-
-
+import os
+import sys
+# find root dir
+root_path = os.path.split((os.path.dirname(__file__)))[0]
+# set root 
+sys.path.append(root_path)
+from Source.oberflaeche import Oberflaeche
 
 root = tkinter.Tk()
 flaeche = Oberflaeche(root)
